8ac3d2ce30f1d7f0f2604bd2cf70e0533d55d991
Aleksey Denisov <deoz@yandex.ru>
2021-03-29 16:15:16 +0300

    Add test coverage

--- src/js/__tests__/MagCharacter.test.js
+++ src/js/__tests__/MagCharacter.test.js
@@ -20,3 +20,17 @@ test('Magician attack distance stoned', () => {
   character.stoned = true;
   expect(character.attack).toBe(72);
 });
+
+test('Magician attack < 0', () => {
+  const character = new Magician('Harry');
+  character.attack = 1;
+  character.distance = 5;
+  character.stoned = true;
+  expect(character.attack).toBe(0);
+});
+
+test('Magician stoned', () => {
+  const character = new Magician('Harry');
+  character.stoned = true;
+  expect(character.stoned).toBe(true);
+});
